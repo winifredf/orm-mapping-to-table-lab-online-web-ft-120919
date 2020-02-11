b0ddecea2796e237e09c482a54da1abe9baf9276
Winifred Frierson <winifred.dykes@hotmail.com>
2020-02-11 09:59:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,7 +23,8 @@ class Student
   def self.drop_table
     sql = <<-SQL
       DROP TABLE students;
-      
+      SQL
+    DB[:conn].execute(sql)
   end
     
   # Remember, you can access your database connection anywhere in this class

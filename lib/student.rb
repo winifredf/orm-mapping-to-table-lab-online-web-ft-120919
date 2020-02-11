2afb4779333d58008526e5aec512eddc75730914
Winifred Frierson <winifred.dykes@hotmail.com>
2020-02-11 10:04:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,6 +26,15 @@ class Student
       SQL
     DB[:conn].execute(sql)
   end
+  
+  def save
+    sql = <<-SQL
+      INSERT INTO songs (name, grade)
+      VALUES (?, ?)
+    SQL
+    
+    DB[:conn].execute(sql, self.name, self.grade)
+  end
     
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  

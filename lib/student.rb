7bfdb856e3dfdda568bbc43eb77a963c08d48ede
Winifred Frierson <winifred.dykes@hotmail.com>
2020-02-11 09:45:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,22 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
   
+  def initialize(name, grade, id=nil)
+    @id = id
+    @name = name
+    @grade = grade
+  end
   
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT
+        grade INTEGER
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
